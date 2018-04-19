0217414a7e6224bd130f7c1a9fad078a0cd1bfbe
wxmimperio <wxmimperio@outlook.com>
2018-04-19 23:55:14 +0800

    add AvroConsumer

--- kafka-java-api/simple-consumer/src/main/java/com/wxmimperio/kafka/AvroConsumer.java
+++ kafka-java-api/simple-consumer/src/main/java/com/wxmimperio/kafka/AvroConsumer.java
@@ -4,8 +4,11 @@ import avro.shaded.com.google.common.collect.Lists;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileStream;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.BinaryDecoder;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.Decoder;
 import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.kafka.clients.CommonClientConfigs;
@@ -14,8 +17,7 @@ import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 import java.util.Properties;
 
@@ -31,17 +33,21 @@ public class AvroConsumer {
         props.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, "10.128.74.83:9092");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test_group");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test_group2");
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 30000);
         props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 10000);
         consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Lists.newArrayList("wxm_test_avro"));
+        consumer.subscribe(Lists.newArrayList("phoenix_test1"));
     }
 
     public void start() throws Exception {
         initKafkaConsumer();
-        String path = "D:\\d_backup\\github\\kafka-best-practice\\kafka-java-api\\simple-producer\\src\\main\\resources\\wxm_test_avro.avsc";
+        String path = "D:\\d_backup\\github\\kafka-best-practice\\kafka-java-api\\simple-producer\\src\\main\\resources\\phoenix_test1.avsc";
         Schema schema = getSchema(path);
+        //Schema schema = SchemaHelper.getSchema("pt_active_glog");
+        //schema = SchemaHelper.getSchema("pt_active_glog");
+
+        System.out.println(schema.toString());
         try {
             while (true) {
                 processRecord(schema, consumer.poll(1000));
@@ -52,7 +58,7 @@ public class AvroConsumer {
     }
 
     private static Schema getSchema(String path) throws Exception {
-        Schema schema = new Schema.Parser().parse(new File(path));
+        Schema schema = new Schema.Parser().parse(new FileInputStream(new File(path)));
         return schema;
     }
 
@@ -73,10 +79,12 @@ public class AvroConsumer {
     }
 
     private String processAvro(Schema schema, ConsumerRecord<String, byte[]> record) throws IOException {
-        byte[] buffer = record.value();
-        BinaryDecoder binaryEncoder = DecoderFactory.get().binaryDecoder(buffer, null);
+        DatumReader<GenericRecord> datumReader = new SpecificDatumReader<GenericRecord>(schema);
+        ByteArrayInputStream byteArrayInputStream = null;
+        byteArrayInputStream = new ByteArrayInputStream(record.value());
+        Decoder decoder = DecoderFactory.get().binaryDecoder(byteArrayInputStream, null);
         GenericRecord gr = null;
-        gr = new SpecificDatumReader<GenericRecord>(schema).read(gr, binaryEncoder);
+        gr = datumReader.read(gr, decoder);
         return gr.toString();
     }
 }
